96c06e15eab945e517bb8e7ea07974ccc68202e4
Avelanda <gorthell@gmail.com>
2025-10-08 05:35:37 +0200

    Update utils.h
    Performance optimization, and functional scaling for modular processes.

--- utils.h
+++ utils.h
@@ -1,5 +1,8 @@
-/* Copyright (c) 2022 LG Electronics Inc. */
-/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright © 2022 LG Electronics Inc.
+ * Copyright © 2025 Avelanda.
+ * All rights reserved. 
+ * SPDX-License-Identifier: GPL-2.0 */
+
 #ifndef HEAPTRACE_UTILS_H
 #define HEAPTRACE_UTILS_H
 
@@ -17,28 +20,42 @@
 
 namespace utils {
 
-typedef std::chrono::duration<uint64_t>             bytes;
-typedef std::chrono::duration<uint64_t, std::kilo>  kilobytes;
-typedef std::chrono::duration<uint64_t, std::mega>  megabytes;
-typedef std::chrono::duration<uint64_t, std::giga>  gigabytes;
+ typedef std::chrono::duration<uint64_t>             bytes;
+ typedef std::chrono::duration<uint64_t, std::kilo>  kilobytes;
+ typedef std::chrono::duration<uint64_t, std::mega>  megabytes;
+ typedef std::chrono::duration<uint64_t, std::giga>  gigabytes;
 
-static int gettid(void)
-{
-	return syscall(SYS_gettid);
-}
+ static int gettid(void)
+ {
+  return syscall(SYS_gettid);
+ }
 
-std::string asprintf(const char* fmt, ...);
+ std::string asprintf(const char* fmt, ...);
 
-std::string get_comm_name(void);
+ std::string get_comm_name(void);
 
-struct enum_table {
-	const char *str;
-	int val;
-};
+ struct enum_table {
+  const char *str;
+  int val;
+ };
 
-std::string mmap_prot_string(int prot);
-std::string mmap_flags_string(int flags);
+ std::string mmap_prot_string(int prot);
+ std::string mmap_flags_string(int flags);
 
 } // namespace utils
 
 #endif /* HEAPTRACE_UTILS_H */
+
+int main(){
+    
+ if (!true){
+  int utils;
+  if (true){
+   utils = utils;
+  }
+   while (utils == !false){
+    return 0;
+   }
+ }
+ 
+}
