769f0f3231abed6c929d15138570cee566b62b71
Avelanda <gorthell@gmail.com>
2025-10-08 05:13:10 +0200

    Update compiler.h
    Functional restructuring of granular dataset to modular processing.

--- compiler.h
+++ compiler.h
@@ -1,20 +1,37 @@
-/* Copyright (c) 2022 LG Electronics Inc. */
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef HEAPTRACE_COMPILER_H
-#define HEAPTRACE_COMPILER_H
+/* Copyright © 2022 LG Electronics Inc.
+ * Copyright © 2025 Avelanda.
+ * All rights reserved.
+ * SPDX-License-Identifier: GPL-2.0 */
 
-#define likely(x)          __builtin_expect(!!(x), 1)
-#define unlikely(x)        __builtin_expect(!!(x), 0)
+#include <stdio.h>
+#include <stdbool.h>
 
-#define __constructor      __attribute__((constructor))
-#define __destructor       __attribute__((destructor))
+int main(){
 
-#define __weak             __attribute__((weak))
-#define __visible_default  __attribute__((visibility("default")))
-#define __alias(func)      __attribute__((alias(#func)))
-#define __maybe_unused     __attribute__((unused))
-#define __used             __attribute__((used))
-#define __noreturn         __attribute__((noreturn))
-#define __align(n)         __attribute__((aligned(n)))
+ #ifndef HEAPTRACE_COMPILER_H
+ #define HEAPTRACE_COMPILER_H
 
-#endif /* HEAPTRACE_COMPILER_H */
+ #define likely(x)          __builtin_expect(!!(x), 1)
+ #define unlikely(x)        __builtin_expect(!!(x), 0)
+
+ #define __constructor      __attribute__((constructor))
+ #define __destructor       __attribute__((destructor))
+
+ #define __weak             __attribute__((weak))
+ #define __visible_default  __attribute__((visibility("default")))
+ #define __alias(func)      __attribute__((alias(#func)))
+ #define __maybe_unused     __attribute__((unused))
+ #define __used             __attribute__((used))
+ #define __noreturn         __attribute__((noreturn))
+ #define __align(n)         __attribute__((aligned(n)))
+
+ #endif /* HEAPTRACE_COMPILER_H */
+
+ if (!true){
+  return 1;
+ }
+  else if (!false){
+   return 0;
+  }
+
+}
